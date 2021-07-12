cb5e97b50543e5548bba5bcc46c9ee89902bde9d
Steve Shilling <stevshil@users.noreply.github.com>
2021-07-12 22:05:02 +0100

    Added config to enable delete

--- registry/start.sh
+++ registry/start.sh
@@ -1,10 +1,34 @@
 #!/bin/bash
 
+cat >/var/tmp/dockerreg_config.yml <<_END_
+version: 0.1
+log:
+  fields:
+    service: registry
+storage:
+  cache:
+    blobdescriptor: inmemory
+  filesystem:
+    rootdirectory: /var/lib/registry
+  delete:
+    enabled: true
+http:
+  addr: :5000
+  headers:
+    X-Content-Type-Options: [nosniff]
+health:
+  storagedriver:
+    enabled: true
+    interval: 10s
+    threshold: 3
+_END_
+
 docker run -itd -p 5000:5000 \
 	--name dockerreg \
 	-v /home/share/dockerreg:/var/lib/registry \
 	-v certs:/certs \
 	--restart=always \
+	-v /var/tmp/dockerreg_config.yml:/etc/docker/registry/config.yml:ro \
 	registry:2
 	#-e REGISTRY_HTTP_TLS_CERTIFICATE=/certs/domain.crt \
   	#-e REGISTRY_HTTP_TLS_KEY=/certs/domain.key \
